567bdeb7a39bd7e089ab05735e4d3964c6495031
rafuj <raihanrafuj9978@gmail.com>
2024-01-26 23:12:28 +0600

    skills added

---
+++ app/components/Skills.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable @next/next/no-img-element */
+
+const Skills = () => {
+	return (
+		<section className="skills-section">
+			<div className="container">
+				<div className="section-title">
+					<h3 className="title">My Skills & Tools</h3>
+					<div className="txt1" style={{ maxWidth: "438px" }}>
+						Highly skilled at progressive <br /> enhancement design
+						systems & UI Engineering
+					</div>
+				</div>
+				<div className="skills">
+					{data.map((item, index) => (
+						<div className="skill" key={index}>
+							<img src={item.img} alt="skill" />
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+const data = [
+	{
+		img: "/img/skills/html.png",
+	},
+	{
+		img: "/img/skills/bootstrap.png",
+	},
+	{
+		img: "/img/skills/firebase.png",
+	},
+	{
+		img: "/img/skills/js.png",
+	},
+	{
+		img: "/img/skills/mongodb.png",
+	},
+	{
+		img: "/img/skills/nodejs.png",
+	},
+	{
+		img: "/img/skills/react.png",
+	},
+	{
+		img: "/img/skills/tailwind.png",
+	},
+	{
+		img: "/img/skills/css.png",
+	},
+	{
+		img: "/img/skills/express.png",
+	},
+	{
+		img: "/img/skills/mui.png",
+	},
+	{
+		img: "/img/skills/next.png",
+	},
+];
+export default Skills;
